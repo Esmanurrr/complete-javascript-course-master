eb4ce0bb4fc5387ed7c67447740c9df8b2dd1a4f
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-08-26 17:04:23 +0300

    Values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,18 @@
 let js = "amazing";
-if(js === "amazing") alert("Javascript is FUN!");
 
 console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM" // dont use & in variable name
+let $function = 27; // dont use reserved words (for example : new, function...)
+let person = "jonas" // dont start with upper case
+let PI = 3.1415; // only constant variables can be upper case
+let myFirstJob = "programmer"
+let myCurrentJob = "teacher" // set your variables clear, not use job1, job2 vs
