af47032bf56a7e7574fe67893800117ca052821a
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-08-30 16:20:26 +0300

    Basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,4 +54,34 @@
  var job = 'programmer';
  job = 'teacher';
 
+ ---- Math Operators ----
+
+ const now = 2037;
+ const ageJonas = now - 1991;
+ const ageSarah = now - 2018;
+ console.log(ageJonas, ageSarah);
+
+ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+ const firstName = 'Jonas';
+ const lastName = 'Schmedtmann';
+ console.log( ${firstName} ${lastName} );
+
+ ---- Assignment Operators ----
+
+ let x = 10 + 5; // = is assignment operator 
+ x+= 10; // x = x + 10
+ x *= 4; // x = x * 4 
+ x++; // x = x + 1
+ x--; // x = x - 1
+
+ --- Comparison operators ----
+ console.log(ageJonas > ageSarah); // >, <, >=, <=
+ console.log(ageSarah >= 18);
+
+ const isFullAge = ageSarah >= 18;
+
+ console.log(now - 1991 > now - 2018); // firstly math operators then comparison operators will run
+
   */
+
