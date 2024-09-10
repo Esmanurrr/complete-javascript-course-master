9a9118b499c57b55a394c4d1f1879cd0a30722a8
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-09-10 22:46:55 +0300

    Switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -211,8 +211,6 @@ if(favourite === 23){ // 23 === 23
 
 if(favourite !== 23) console.log('Why not 23?');
 
-*/
-
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
 
@@ -227,4 +225,31 @@ if(hasDriversLicense && hasGoodVision && !isTired){
     console.log('Sarah is able to drive');
 }else {
     console.log('Someone else should drive...');
+}
+
+*/
+
+const day = 'monday';
+
+switch(day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meet up');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
 }
