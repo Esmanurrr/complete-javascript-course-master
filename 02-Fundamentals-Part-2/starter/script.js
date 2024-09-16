9bc37e0d19f4e39d4142028768c64b3846802129
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-09-16 23:13:32 +0300

    Introduction of functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,14 +2,38 @@
 // 1 - strict mod bazı şeyleri yapmamızı yasaklar.
 // 2 - strict mod olmadan JS'in hata yaptığımızı bize bildirmeden sessizce başarısız olacağı durumları bizim için görünür hatalar oluşturur.
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriverLicense = true; // when strict mode is close, we cant see the error on the console but there is an error
-if (hasDriversLicense) console.log('I can drive'); 
+ // if(passTest) hasDriverLicense = true; when strict mode is close, we cant see the error on the console but there is an error
+// if (hasDriversLicense) console.log('I can drive'); 
 
 // strict mode see the error of reserved word
 
 // const interface = 'Audio'; 
 // const private = 423;
 // const if = 23;
+
+// --- Functions ---
+
+function logger(){
+    console.log("My name is Esmanur");
+}
+
+//calling, running or invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0); // sadece bunu çalıştırdığımızda kendi içerisinde bu sonucu döndü ama biz onu konsola yazdırmadık. 
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
