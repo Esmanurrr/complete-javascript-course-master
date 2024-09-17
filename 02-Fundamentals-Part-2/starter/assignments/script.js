258fc91858033e8c6ef7361b1fb00b919f3fb7c4
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-09-17 11:34:55 +0300

    added function declaration and expression assignment

--- 02-Fundamentals-Part-2/starter/assignments/script.js
+++ 02-Fundamentals-Part-2/starter/assignments/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
-function describeCountry(country, population, capitalCity){
-    return `${country} has ${population} million people and its capital city is ${capitalCity} \n`;
+// function describeCountry(country, population, capitalCity){
+//     return `${country} has ${population} million people and its capital city is ${capitalCity} \n`;
+// }
+
+// const turkey = describeCountry("Turkey", 85, "Ankara");
+// const korea = describeCountry("South Korea", 51, "Seoul");
+// const norvey = describeCountry("Norvey", 5, "Oslo");
+
+// console.log(turkey, korea, norvey);
+
+function percentageOfWorld1(population){
+    const worldPop = 7900;
+    return ((population / worldPop) * 100).toFixed(1);
 }
 
-const turkey = describeCountry("Turkey", 85, "Ankara");
-const korea = describeCountry("South Korea", 51, "Seoul");
-const norvey = describeCountry("Norvey", 5, "Oslo");
+percentageOfWorld1(85);
+percentageOfWorld1(5);
+percentageOfWorld1(1441);
 
-console.log(turkey, korea, norvey);
+
+const percentageOfWorld2 = function(population){
+    const worldPop = 7900;
+    return ((population / worldPop) * 100).toFixed(1);
+}
+
+const turkey = percentageOfWorld2(85);
+const norvey = percentageOfWorld2(5);
+const china = percentageOfWorld2(1441);
+
+
+console.log(turkey, norvey, china);
