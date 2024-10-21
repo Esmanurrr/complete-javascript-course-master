89b60269a2aefe5aee2fca9e93e9447f2745534a
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-10-21 09:06:43 +0300

    Destructing arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order : function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,42 @@ const restaurant = {
     },
   },
 };
+
+// Destructing array is simply seperating variables of array (original array will not affected)
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr; // arr[0] = x, arr[1] = y, arr[2] = z
+
+// const [first, second] = restaurant.categories; // Italian Pizzeria
+
+// const [first, , third] = restaurant.categories; // Italian Vegetarian
+
+// let [main, , secondary] = restaurant.categories; // Italian Vegetarian
+// console.log(main, secondary);
+
+// ---- Switcing variables without destructing: 
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+ 
+// with destructing:
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// Recieve 2 return values from a function
+const [starter, main] = restaurant.order(2, 0); // Garlic Bread Pizza
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested; // 2 [5,6]
+const [i, ,[j,k]] = nested; // 2 5 6
+
+// Default Values
+// const [p, q, r] = [8, 9]; // p = 8, q = 9, r = undefined;
+const [p = 1, q = 1, r = 1] = [8, 9]; // p = 8, q = 9, r = 1;
+
+
