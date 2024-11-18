12e122e9d94caaa0a2e57ca3913908d7cace834d
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-11-18 15:27:32 +0300

    the call and apply methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -112,10 +112,61 @@
 //     };
 // };
 
-const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+// const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
-const greeterHey = greet('Hey');
-greeterHey('Jonas');
-greeterHey('Steven');
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+// greeterHey('Steven');
 
-greet('Hello')('Jonas');
+// greet('Hello')('Jonas');
+
+
+
+// ------------------- THE CALL AND APPLY METHODS ---------------
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNum, name){
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name })
+    },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: []
+}
+
+const book = lufthansa.book; // seperate function from lufthansa's book function
+
+// does not work
+// book(23, 'Sarah William'); // Uncaught TypeError: Cannot property 'airline' of undefined 
+
+// Call Method 
+book.call(eurowings, 23, 'Sarah Williams'); // Sarah Williams booked a seat on Eurowings flight EW23
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper'); // Mary Cooper booked a seat on Lufthansa flight LH239
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    bookings: []
+}
+
+book.call(swiss, 583, 'Mary Cooper'); // Mary Cooper booked a seat on Swiss Air Lines flight LX583
+console.log(swiss);
+
+// Apply Method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData); // apply method should get an array as an argument
+console.log(swiss);
+
+book.call(swiss, ...flightData); // call method takes parameters individually when calling a function.
