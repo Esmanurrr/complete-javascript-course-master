7c76ddabeeb75d2708901df9e8a7027709776b8c
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-12-01 12:02:37 +0300

    numeric separators

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -335,6 +335,7 @@ console.log(+(2.345).toFixed(2)); // 2.35 (number)
 
 // ------------- THE REMAINDER OPERATOR -------------
 
+/*
 console.log(5 % 2); // 1
 console.log(5 / 2); // 2.5
 
@@ -360,3 +361,22 @@ labelBalance.addEventListener('click', function(){
     if(i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+*/
+
+// ------------- THE NUMERIC SEPERATORS  -------------
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000')); // NaN
+console.log(parseInt('230_000')); // 230
